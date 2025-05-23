a94a8e528beef0c28ca6dc6f05c228d69ae7a055
hamis5555 <hamisemad@gmail.com>
2025-05-24 00:46:02 +0300

    Remove docs folder

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vaultix-media-library/',
   plugins: [react()],
-  build: {
-  },
 })
