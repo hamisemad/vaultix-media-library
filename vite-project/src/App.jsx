38fe9fc28982373f092dac45f6dc99e80e26973e
hamis5555 <hamisemad@gmail.com>
2025-05-24 03:09:50 +0300

    Fix: Rename productionHouse to ProductionHouse to match import casing

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Slider from './components/Slider'
-import ProductionHouse from './components/productionHouse'
+import ProductionHouse from './components/ProductionHouse'
 import GenreMoviesList from './components/GenreMoviesList'
 import SignUpModal from './components/SignUpModal'
 import MoviePage from './components/MoviePage';
