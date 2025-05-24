08aa10629170bdde6bc9ad7168466528a0cec46a
Hamis Emad Eldeen <hamisemad8@gmail.com>
2025-05-24 03:47:21 +0300

    Update Header.jsx

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -218,7 +218,7 @@ function Header({ onProfileClick }) {
 
       <img
         src={profilePic}
-        className='w-12 p-3 m-3 rounded-full m-3 cursor-pointer'
+        className='w-12 p-3 rounded-full m-5 cursor-pointer'
         onClick={onProfileClick}
       />
     </div>
